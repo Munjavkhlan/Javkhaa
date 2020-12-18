7794fbf496e81825375c062035219943ff0b3812
Munjavkhlan <74762102+Munjavkhlan@users.noreply.github.com>
2020-12-18 17:48:34 +0800

    lab4

---
+++ lab4/p5.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Example from './components/example/Example';
+import States from './components/states/States';
+import Header from './components/header/Header';
+import {HashRouter, Route, Link } from "react-router-dom";
+import './p5.css';
